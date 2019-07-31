052c4db23b62c1b8c7c5ed609a73d3193f94387c
Remi Gau <remi_gau@hotmail.com>
2019-07-31 08:39:21 +0200

    greatly improve plotting results

---
+++ avg_beh_results.m
@@ -0,0 +1,141 @@
+% plots the average across subjects of:
+% - stimulus onsets / offsets (to make sure that there is not too much
+% variation between subjects
+% - average across subject of the time course of each response type.
+% - this can be row normalized for each subject (by the sum of response for
+% that subject on that run - gives more weight to subjects with more SNR in their
+% response)
+% - it is possible to bin the responses from their original 25 Hz sampling
+% frequency.
+% - responses can be passed through a moving with window size
+
+clear
+clc
+
+% mention where the BIDS data set is (can get the behavioral from OSF)
+tgt_dir = 'D:\BIDS\olf_blind\raw';
+
+% if needed add spm to the path
+% spm_path = '/home/remi-gau/Documents/SPM/spm12';
+% addpath(spm_path)
+
+spm('defaults','fmri')
+
+addpath(genpath(fullfile(pwd, 'subfun')))
+
+% loads bids data
+bids =  spm_BIDS(tgt_dir);
+
+%% plotting
+opt.bin_size = 10; % number of data point to bin. e.g. 25: means 1 sec of responses are summed 
+
+opt.plot_subj = 0; % plot subjects - usualluy not pretty
+
+opt.mov_mean_resp = 1; % use a moving window to average
+opt.moving_win_size = 20; % width of the moving window
+
+% limit of y axis when plotting both groups
+max_y_axis = [...
+        0.105;... % with no row normalization
+        0.015 ]/1.5; % with row normalization
+
+% do the loading of the data and plot
+close all
+
+out_dir = fullfile('output', 'figures', 'beh_avg');
+mkdir(out_dir)
+
+tasks = spm_BIDS(bids, 'tasks');
+tasks(3) = [];
+
+group(1).name = 'blnd';
+group(2).name = 'ctrl';
+
+opt.visible = 'on';
+
+if ~opt.mov_mean_resp 
+    opt.moving_win_size = 0;
+end
+
+for iTask = 1:numel(tasks)
+    
+    for iGroup = 1:numel(group)
+        
+        opt.group = group(iGroup).name;
+        
+        opt.plot = 'Run';
+        opt.max_y_axis = [];
+        
+        subjects = spm_BIDS(bids, 'subjects', ...
+            'task', tasks{iTask});
+        
+        idx = strfind(subjects, {group(iGroup).name});
+        idx = find(~cellfun('isempty', idx)); %#ok<STRCL1>
+        
+        subjects(idx)
+        
+        [all_stim, task] = average_beh(bids, tasks{iTask}, subjects(idx));
+        
+        % average data across runs
+        for iTrialType = 1:size(all_stim,1)
+            avg_all_stim{iTrialType,1} = ...
+                mean( ...
+                cat(3, ...
+                all_stim{iTrialType,1},  ...
+                all_stim{iTrialType,2}), ...
+                3); %#ok<SAGROW>
+            % keep a copy for plotting both groups later
+            bothgrp_avg_all_stim{iTrialType,iGroup} = ...
+                mean( ...
+                cat(3, ...
+                all_stim{iTrialType,1},  ...
+                all_stim{iTrialType,2}), ...
+                3); %#ok<SAGROW>
+        end
+
+        % plot each group with and without row normalization
+        for iNorm = 0:1
+            opt.norm_resp = iNorm;
+            
+            % plot the 2 runs separately
+            prefix = 'beh_grp-';
+            make_figure(all_stim, task, opt)
+            mtit(sprintf('Group: %s ; Task: %s ; Row norm: %i', ...
+                opt.group, tasks{iTask}, opt.norm_resp), ....
+                'fontsize', 14, 'xoff', 0,'yoff',0.05);
+            print_fig(prefix, out_dir, group(iGroup).name, task, iNorm)
+            
+            % plot the run averages
+            prefix = 'beh_runavg_grp-';
+            make_figure(avg_all_stim, task, opt)
+            mtit(sprintf('Run avg ; Group: %s ; Task: %s ; Row norm: %i', ...
+                opt.group, tasks{iTask}, opt.norm_resp), ....
+                'fontsize', 14, 'xoff', 0,'yoff',0.05);
+            print_fig(prefix, out_dir, group(iGroup).name, task, iNorm)
+        end
+
+    end
+    
+    opt.plot = 'Group';
+    opt.max_y_axis = max_y_axis;
+    
+    % plot both groups with and without row normalization
+    for iNorm = 0:1
+        opt.norm_resp = iNorm;
+        
+        % plot the 2 groups separately
+        prefix = 'beh_bothgrp-';
+        make_figure(bothgrp_avg_all_stim, task, opt)
+        mtit(sprintf('Group: %s ; Task: %s ; Row norm: %i', ...
+            'both', tasks{iTask}, opt.norm_resp), ....
+            'fontsize', 14, 'xoff', 0,'yoff',0.05);
+        print_fig(prefix, out_dir, group(iGroup).name, task, iNorm)
+        
+    end
+    
+end
+
+function print_fig(prefix, out_dir, group_name, task, iNorm)
+print(gcf, fullfile(out_dir, ...
+    [prefix group_name '_task-' task '_norm-' num2str(iNorm) '.jpg']), '-djpeg')
+end
--- subfun/make_figure.m
+++ subfun/make_figure.m
@@ -1,14 +1,15 @@
 function make_figure(all_stim, tasks, opt)
 
-fontsize = 12;
+% trial_type = {...
+%     'ch1', ...
+%     'ch3', ...
+%     'ch5', ...
+%     'ch7', ...
+%     'resp_03', ...
+%     'resp_12'};
 
-trial_type = {...
-    'ch1', ...
-    'ch3', ...
-    'ch5', ...
-    'ch7', ...
-    'resp_03', ...
-    'resp_12'};
+fontsize = 12;
+samp_freq = 25;
 
 stim_color = {...
     '-g';...  % Eucalyptus Left
@@ -22,78 +23,136 @@ Legend = {...
     'Euc - Right';...
     'Alm - Right'};
 
+black = [0 0 0];
+blnd_color = [255 158 74];
+sighted_color = [105 170 153];
+
 % take latest offset of the last stim to set the end of the run for
 % plottingg
-run_length = find(any(all_stim{4,1}==-1), 1, 'last'); 
+run_length = find(any(all_stim{4,1}==-1), 1, 'last');
 
 nb_bins = floor(run_length/opt.bin_size);
 
-nb_subj = size(all_stim{1,1},1);
+nb_columns = size(all_stim,2);
 
-if opt.norm_resp
-    norm_text = 'YES';
-    y_tick = 0:.01:1;
-    y_label = 0:.01:1;
-else
-    norm_text = 'NO';
-    one_resp = 1/nb_subj;
-    y_tick = 0:one_resp:1;
-    y_label = 0:1:numel(y_tick);
+opt.nb_subj = size(all_stim{1,1},1);
+
+bin_duration  = 1/samp_freq * opt.bin_size;
+x_tick = round(linspace(0,nb_bins,10));
+x_label = round(linspace(0,nb_bins*bin_duration,10));
+
+switch nb_columns
+    case 1
+        SubPlot = [1 2 3];
+        if strcmp(opt.plot, 'Run')
+            Run = {'1 & 2'};
+            Color = repmat(black,[3,1])/255;
+        else
+            Run = {opt.group};
+            %             Color = repmat(black,[3,1]);
+        end
+    case 2
+        SubPlot = [1 3 5 2 4 6];
+        if strcmp(opt.plot, 'Run')
+            Run = {'1','2'};
+            Color = repmat(black,[6,1])/255;
+        else
+            Run = {'blnd' 'ctrl'};
+            Color = [...
+                black; blnd_color; blnd_color;...
+                black; sighted_color; sighted_color]/255;
+        end
 end
 
-
+%%
 figure('name', tasks, 'position', [50 50 1300 700], 'visible', opt.visible)
 
-for iRun = 1:2
+
+iSubplot = 1;
+
+for iRun = 1:nb_columns
     
-    % plot stimuli: each type with a different color
-    subplot(3,2,0+iRun)
-    hold on
+    %% Plot stimuli: each type with a different color
+    
+    opt.mov_mean = 0;
     opt.normalize = 0;
+    
+    subplot(3, nb_columns, SubPlot(iSubplot))
+    hold on
+    
     for iStim = 1:4
         to_plot = all_stim{iStim,iRun}(:,1:run_length);
         to_plot = process_timeSeries(to_plot, nb_bins, opt);
         plot(to_plot, stim_color{iStim}, 'linewidth', 2)
     end
     axis tight
-    set(gca, 'fontsize', fontsize)
-    t = title(sprintf('Group: %s ; Task: %s ; Run %i ; Bin size: %i', ...
-        opt.group, tasks, iRun, opt.bin_size));
+    set(gca, 'fontsize', fontsize, 'xtick', x_tick)
+    t = title(sprintf('%s %s ; Bin size: %i ; Mov win size: %i', ...
+        opt.plot, Run{iRun}, opt.bin_size, opt.moving_win_size));
     set(t, 'fontsize', fontsize)
     ylabel('Stimuli')
     legend(Legend)
     
-    subplot(3,2,2+iRun)
-    hold on
+    iSubplot = iSubplot + 1;
+    
+    
+    %% Plot response 1
+    
+    opt.mov_mean = opt.mov_mean_resp;
     opt.normalize = opt.norm_resp;
+    
+    subplot(3, nb_columns, SubPlot(iSubplot))
+    hold on
+    
     to_plot = all_stim{5,iRun}(:,1:run_length);
-    to_plot = process_timeSeries(to_plot, nb_bins, opt);
-    plot(to_plot, 'k', 'linewidth', 2)
-    axis tight
-    set(gca, 'fontsize', fontsize, ...
-        'ytick', y_tick, 'yticklabel', y_label)
+    [to_plot, sem, all_subjs] = process_timeSeries(to_plot, nb_bins, opt);
+    do_plot(to_plot, sem, all_subjs, Color, iSubplot, opt)
+    [norm_text] = label_your_axes(fontsize, x_tick, x_label, opt); % or ou might lose them: an old dwarf saying
     ylabel(sprintf('Resp 1 - Row norm: %s', norm_text))
     
-    subplot(3,2,4+iRun)
+    iSubplot = iSubplot + 1;
+    
+    %% Plot response 2
+    subplot(3, nb_columns, SubPlot(iSubplot))
     hold on
-    opt.normalize = opt.norm_resp;
+    
     to_plot = all_stim{6,iRun}(:,1:run_length);
-    to_plot = process_timeSeries(to_plot, nb_bins, opt);
-    plot(to_plot, 'k', 'linewidth', 2)
-    axis tight
-    set(gca, 'fontsize', fontsize, ...
-        'ytick', y_tick, 'yticklabel', y_label)
+    [to_plot, sem, all_subjs] = process_timeSeries(to_plot, nb_bins, opt);
+    do_plot(to_plot, sem, all_subjs, Color, iSubplot, opt)
+    [norm_text] = label_your_axes(fontsize, x_tick, x_label, opt);
     ylabel(sprintf('Resp 2 - Row norm: %s', norm_text))
+    
+    iSubplot = iSubplot + 1;
     
 end
 end
 
-function to_plot = process_timeSeries(to_plot, nb_bins, opt)
+function do_plot(to_plot, sem, all_subjs, Color, iSubplot, opt)
+% either plot mean and each subject or mean +/- sem
+if opt.plot_subj
+    for iSubj = 1:size(all_subjs,1)
+        plot(1:length(to_plot), all_subjs(iSubj,:), ...
+            'color', Color(iSubplot,:)*.9, 'linewidth', .5);
+        plot(1:length(to_plot), to_plot, ...
+            'color', Color(iSubplot,:), 'linewidth', 2);
+    end
+else
+    shadedErrorBar(1:length(to_plot), to_plot, sem, ...
+        {'color', Color(iSubplot,:), 'linewidth', 2})
+end
+end
+
+function [to_plot, sem, all_subjs] = process_timeSeries(to_plot, nb_bins, opt)
 to_plot(isnan(to_plot)) = 0;
 to_plot = bin_data(to_plot, nb_bins, opt);
 if opt.normalize
     to_plot = normalize_data(to_plot);
 end
+if opt.mov_mean
+    to_plot = movmean(to_plot,opt.moving_win_size,2);
+end
+all_subjs = to_plot;
+sem = nanstd(to_plot)/size(to_plot,1);
 to_plot = nanmean(to_plot);
 end
 
@@ -119,4 +178,32 @@ function to_plot = normalize_data(to_plot)
 for iRow = 1:size(to_plot,1)
     to_plot(iRow,:) = to_plot(iRow,:)/sum(to_plot(iRow,1:end));
 end
+end
+
+function [norm_text] = label_your_axes(fontsize, x_tick, x_label, opt)
+
+if opt.norm_resp
+    norm_text = 'YES';
+    y_tick = 0:.0025:.1;
+    y_label = 0:.0025:.1;
+else
+    norm_text = 'NO';
+    one_resp = 1/opt.nb_subj;
+    y_tick = 0:one_resp/4:1;
+    y_label = 0:.5:numel(y_tick);
+end
+
+axis tight
+set(gca, 'fontsize', fontsize, ...
+    'ytick', y_tick, 'yticklabel', y_label, ...
+    'xtick', x_tick, 'xticklabel', x_label, ...
+    'ticklength', [.02 .02], 'tickdir', 'out')
+
+if ~isempty(opt.max_y_axis)
+    max_y_axis = opt.max_y_axis(opt.normalize+1);
+    ax = axis;
+    axis([ax(1) ax(2) 0 max_y_axis]);
+end
+
+xlabel('time (sec)')
 end

aaee1818ae25c8a9f6419f2c1c2f3a2adf5372ef
Remi Gau <remi_gau@hotmail.com>
2019-08-22 10:59:30 +0200

    add spaces to to step 3

--- step_3_get_SOT.m
+++ step_3_get_SOT.m
@@ -32,64 +32,64 @@ if ~exist('subj_to_do', 'var')
 end
 
 for iTask = 1:numel(tasks)
-    
+
     % we take each event file from each subject for that task and we collect
     % onsets and offsets of all stim and responses
     for iSubjects = 1:numel(subjects)
-        
+
         runs = spm_BIDS(bids, 'runs', ...
             'type', 'events', ...
             'sub', subjects{iSubjects}, ...
             'task', tasks{iTask});
-        
+
         for iRun = 1:numel(runs)
-            
+
             event_file = spm_BIDS(bids, 'data', ...
                 'type', 'events', ...
                 'sub', subjects{iSubjects}, ...
                 'task', tasks{iTask}, ...
                 'run', runs{iRun});
-            
+
             if numel(event_file)>1
                 error('We should have only one file.')
             end
-            
+
             % we collect the stim / resp onsets and offsets
             %get events file
             disp(event_file)
             x = spm_load(event_file{1},'',1);
-            
+
             % Get the unique names of the conditions (removing repetitions)
             names = unique(x.trial_type(:))';
             nb_cdt = length(names);
-            
+
             if length(names)<6
                 warning('We are missing conditions.')
                 disp(names)
             end
-            
+
             % Create empty variables of onsets and durations
             onsets = cell(1,nb_cdt) ;
             durations = cell(1,nb_cdt) ;
-            
+
             % we collect the stim / resp onsets and offsets
             for iCdt = 1:nb_cdt
-                
+
                 idx = strcmp(x.trial_type, names{iCdt});
-                
+
                 onsets{1,iCdt} = x.onset(idx);
-                
+
                 if isempty(onsets{1,iCdt})
                     warning('One condition has no onset.')
                     dips(names{iCdt})
                 end
-                
+
                 durations{1,iCdt} = x.duration(idx);
-                
+
             end
-            
+
             opt = get_option;
-            
+
             % rename conditions to make them easier to ID later
             % variable used to check that we have all 4 stims for that run
             % if not the .mat filename is flagged as MIA
@@ -113,21 +113,21 @@ for iTask = 1:numel(tasks)
                 end
                 names{iCdt} = strrep(names{iCdt}, '_', '-');
             end
-            
+
             if all(is_here)
                 suffix = '.mat';
             else
                 suffix = '_MIA.mat';
             end
-            
+
             [path, filename] = spm_fileparts(event_file{1});
             % save the onsets as a matfile
             save(fullfile(output_dir, ['sub-' subjects{iSubjects}], ...
                 'func', [filename suffix]),...
                 'names','onsets','durations');
-            
+
         end
-        
+
     end
-    
+
 end

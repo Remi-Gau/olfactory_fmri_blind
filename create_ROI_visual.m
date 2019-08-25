c7671e47201e6250ac11316cf42fee688f3ce6bb
Remi Gau <remi_gau@hotmail.com>
2019-08-25 12:27:28 +0200

    now create a mask for each visual ROI

--- create_ROI_visual.m
+++ create_ROI_visual.m
@@ -1,4 +1,4 @@
-% create visual ROIS
+% create visual ROIS based on retinotoic atlased from Kastner's lab
 
 % URL = 'http://scholar.princeton.edu/sites/default/files/napl/files/probatlas_v4.zip';
 unzip(fullfile(pwd, 'inputs', 'probatlas_v4.zip'), fullfile(pwd, 'inputs'));
@@ -32,39 +32,54 @@ gunzip(fullfile(pwd, 'inputs', 'ProbAtlas_v4', 'subj_vol_all', 'maxprob_vol_*h.n
 % 24 , ... SPL1	 
 % 25 , ... FEF
 
-ROI_2_select = [
-    01 , ... V1v
-    02 , ... V1d
-    03 , ... V2v
-    04 , ... V2d
-    05 , ... V3v
-    06 , ... V3d
-    07 , ... hV4
-    08 , ... VO1
-    09 , ... VO2
-    10 , ... PHC1
-    11 , ... PHC2
-    12 , ... MST
-    13 , ... hMT
-    14 , ... LO2
-    15 , ... LO1
-    16 , ... V3b
-    17 , ... V3a
-];
+ROI_names = { ...
+'V1v'
+'V1d'
+'V2v'
+'V2d'
+'V3v'
+'V3d'
+'hV4'
+'VO1'
+'VO2'
+'PHC1'
+'PHC2'
+'MST'
+'hMT'
+'LO2'
+'LO1'
+'V3b'
+'V3a'};
 
+for iROI = 1:numel(ROI_names)
+    ROIs_2_select(iROI).label = iROI; %#ok<*SAGROW>
+    ROIs_2_select(iROI).name = ROI_names{iROI};
+end
 
+%% read maximum probability map (sum both hemispheres)
 files =  spm_select('FPList', fullfile(pwd, 'inputs'), '^maxprob_vol_.*h.nii$');
 hdr = spm_vol(files);
 vol = spm_read_vols(hdr);
 vol = sum(vol,4);
 
+%% Create an image for each ROI and one that combines them all
+hdr = hdr(1);
+
 all_rois = zeros(size(vol));
 
-for i = 1:numel(ROI_2_select)
-    all_rois(vol==ROI_2_select(i)) = 1;
+for i = 1:numel(ROIs_2_select)
+    % save this ROI
+    this_roi = zeros(size(vol));
+    this_roi(vol==ROIs_2_select(i).label) = 1;
+    hdr.fname = fullfile(pwd, 'inputs', ...
+        ['ROI-' ROIs_2_select(i).name ' _space-MNI.nii']);
+    spm_write_vol(hdr, this_roi);
+    
+    % aupdate all ROIs
+    all_rois(vol==ROIs_2_select(i).label) = 1;
 end
 
-hdr = hdr(1);
-hdr.fname = fullfile(pwd, 'inputs', 'all_visual_ROIS.nii');
+ % save all ROIs
+hdr.fname = fullfile(pwd, 'inputs', 'ROI-AllVisual_space-MNI.nii');
 spm_write_vol(hdr, all_rois);
 

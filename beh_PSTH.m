81a2083dc45d8b899f9436e4b22a732389a47e8a
Remi Gau <remi_gau@hotmail.com>
2019-08-26 13:22:49 +0200

    refactor beh_PSTH

--- beh_PSTH.m
+++ beh_PSTH.m
@@ -13,22 +13,53 @@ clc
 % mention where the BIDS data set is (can get the behavioral from OSF)
 tgt_dir = 'D:\BIDS\olf_blind\raw_beh';
 
+rm_baseline = 0;
+
+baseline_dur = 20;
+pre_stim = 16;
+stim_dur = 16;
+post_stim = 16;
+
+
+% some settings for the figures
+max_y_axis = 6.5;
+
+opt.bin_size = 20;
+
+opt.mov_mean = 1;
+opt.moving_win_size = 20;
+
+opt.max_y_axis = ones(2,1)*.15;
+
+opt.plot_subj = 0;
+opt.normalize = 0;
+opt.visible = 'on';
+fontsize = 12;
+
+% just to prevent label_your_axes from trying to give a meaningful scale
+opt.norm_resp = 1;
+
+
 % if needed add spm to the path
 % spm_path = '/home/remi-gau/Documents/SPM/spm12';
 % addpath(spm_path)
+
+% check that spm is there
 spm('defaults','fmri')
 
 addpath(genpath(fullfile(pwd, 'subfun')))
 
-% loads bids data
-bids =  spm_BIDS(tgt_dir);
+
 
 %% get data
+
+% loads bids data
+bids =  spm_BIDS(tgt_dir);
 
 % do the loading of the data and plot
 close all
 
-out_dir = fullfile('output', 'figures', 'beh_avg');
+out_dir = fullfile(pwd, 'output', 'figures', 'beh_avg');
 mkdir(out_dir)
 
 tasks = spm_BIDS(bids, 'tasks');
@@ -71,18 +102,215 @@ end
 
 opt = get_option;
 
-rm_baseline = 0;
-
-baseline_dur = 20;
-pre_stim = 16;
-stim_dur = 16;
-post_stim = 16;
-
 baseline_dur = baseline_dur * opt.samp_freq;
 pre_stim = pre_stim * opt.samp_freq;
 stim_dur = stim_dur * opt.samp_freq;
 post_stim = post_stim * opt.samp_freq;
 
+[prestim_epoch, stim_epoch, poststim_epoch] = epoch_data(tasks, group, all_time_courses, baseline_dur, pre_stim, post_stim);
+
+
+
+%% Plots the PSTH
+clc
+close all
+
+opt = get_option(opt);
+stim_color_mat = opt.stim_color_mat;
+stim_color_mat(stim_color_mat==0) = .8;
+stim_linestyle = opt.stim_linestyle;
+stim_legend = opt.stim_legend;
+samp_freq = opt.samp_freq;
+
+blnd_color = opt.blnd_color;
+sighted_color = opt.sighted_color;
+black = [0 0 0];
+
+bin_duration  = 1/samp_freq * opt.bin_size;
+
+x1_patch = pre_stim/opt.bin_size;
+x2_patch = (pre_stim+stim_dur)/opt.bin_size;
+
+
+for iGroup = 1:2
+    
+    if iGroup==1
+        Color = blnd_color/255;
+    else
+        Color = sighted_color/255;
+    end
+    
+    for iTask = 1:2
+        
+        figure('name', ['PSTH - ' tasks{iTask}], ...
+            'position', [50 50 1300 700], ...
+            'visible', opt.visible)
+        
+        iSubplot = 1;
+        
+        for iResp = 1:2
+            for iTrialtype = 1:4
+                
+                to_plot = cat(2, ...
+                    prestim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype),...
+                    stim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype),...
+                    poststim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype) );
+                
+                opt.nb_subj = size(to_plot,2);
+                
+                nb_bins = round(length(to_plot)/opt.bin_size);
+                x_tick = round(linspace(0,nb_bins,10));
+                x_label = round(linspace(0,nb_bins*bin_duration,10)) - pre_stim/samp_freq;
+                
+                [to_plot, sem, all_subjs] = process_timeSeries(to_plot, nb_bins, opt);
+                
+                subplot(2,4, iSubplot)
+                hold on
+                
+                patch(...
+                    [x1_patch x1_patch x2_patch x2_patch],...
+                    [0 opt.max_y_axis(1) opt.max_y_axis(1) 0],...
+                    stim_color_mat(iTrialtype,:),...
+                    'linestyle', stim_linestyle{iTrialtype});
+ 
+                do_plot(to_plot, sem, all_subjs, Color, 1, opt)
+
+                plot([0 numel(to_plot)], [0 0], 'k')
+                
+                label_your_axes(fontsize, x_tick, x_label, opt);
+                
+                axis tight
+                ax = axis;
+                axis([ax(1) ax(2) ax(3) opt.max_y_axis(1)]);
+                
+                iSubplot = iSubplot + 1;
+            end
+            
+        end
+        
+        subplot(2, 4, 1)
+        t = ylabel('Resp 1  - [A U]');
+        set(t, 'fontsize', fontsize);
+        
+        subplot(2, 4, 5)
+        t = ylabel('Resp 2 - [A U]');
+        set(t, 'fontsize', fontsize);
+        
+        for iTrialtype = 1:4
+            subplot(2, 4, iTrialtype)
+            title(stim_legend{iTrialtype});
+        end
+        
+        mtit(sprintf('Group: %s ; Task: %s ; Bin size: %i ; Mov win size: %i', ...
+            group(iGroup).name, tasks{iTask}, opt.bin_size, opt.moving_win_size),....
+            'fontsize', 14, 'xoff', 0,'yoff',0.05);
+        
+        print(gcf, fullfile(out_dir, ...
+            ['PSTH_' ...
+            group(iGroup).name ...
+            '_task-' tasks{iTask} ...
+            '_rmbase-' num2str(rm_baseline) ...
+            '.jpg']), '-djpeg')
+        
+    end
+end
+
+
+%% Plots the average number of response during stimulus period
+clc
+close all
+
+
+opt = get_option(opt);
+stim_color_mat = opt.stim_color_mat;
+stim_linestyle = opt.stim_linestyle;
+stim_legend = opt.stim_legend;
+Colors(1,:) = opt.blnd_color;
+Colors(2,:) = opt.sighted_color;
+Colors = Colors/255;
+
+for iTask = 1:2
+    
+    figure('name', tasks{iTask}, 'position', [50 50 1300 700], 'visible', opt.visible)
+    
+    iSubplot = 1;
+    
+    for iResp = 1:2
+        for iTrialtype = 1:4
+            
+            clear to_plot
+            
+            subplot(2,4, iSubplot)
+            hold on
+            
+            for iGroup = 1:2
+                
+                to_plot = sum(stim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype),2);
+                
+                h = plotSpread(to_plot, 'distributionIdx', ones(size(to_plot)), ...
+                    'distributionMarkers',{'o'},...
+                    'distributionColors',Colors(iGroup,:), ...
+                    'xValues', iGroup, ...
+                    'showMM', 0, ...
+                    'binWidth', .1, 'spreadWidth', 1);
+
+                set(h{1}, 'MarkerSize', 7, 'MarkerEdgeColor', 'k', ...
+                    'MarkerFaceColor', Colors(iGroup,:), ...
+                    'LineWidth', 2)
+                
+                h = errorbar(iGroup-.5, ...
+                    mean(to_plot), ...
+                    std(to_plot)/numel(to_plot)^.5,...
+                    'o',...
+                    'color', Colors(iGroup,:), 'linewidth', 2,...
+                    'MarkerSize', 5, ...
+                    'MarkerEdgeColor', 'k', ...
+                    'MarkerFaceColor', Colors(iGroup,:));
+ 
+            end
+           
+            plot([0 3], [0 0], 'k')
+            
+            set(gca, 'fontsize', fontsize, ...
+                'ytick', 0:1:6, 'yticklabel', 0:1:6, ...
+                'xtick', 1:2, 'xticklabel', {group.name}, ...
+                'ticklength', [.02 .02], 'tickdir', 'out')
+            
+            axis([0.2 2.5 0 max_y_axis]);
+            
+            iSubplot = iSubplot + 1;
+        end
+        
+    end
+    
+    subplot(2, 4, 1)
+    t = ylabel('Resp 1  - [A U]');
+    set(t, 'fontsize', fontsize);
+    
+    subplot(2, 4, 5)
+    t = ylabel('Resp 2 - [A U]');
+    set(t, 'fontsize', fontsize);
+    
+    for iTrialtype = 1:4
+        subplot(2, 4, iTrialtype)
+        title(stim_legend{iTrialtype});
+    end
+    
+    mtit(sprintf('Mean number of responses during stim epoch ; Task: %s', tasks{iTask}),....
+        'fontsize', 14, 'xoff', 0,'yoff',0.05);
+    
+    print(gcf, fullfile(out_dir, ...
+        ['Avg_Resp_' ...
+        group(iGroup).name ...
+        '_task-' tasks{iTask} ...
+        '_rmbase-' num2str(rm_baseline) ...
+        '.jpg']), '-djpeg')
+    
+end
+
+
+
+function [prestim_epoch, stim_epoch, poststim_epoch] = epoch_data(tasks, group, all_time_courses, baseline_dur, pre_stim, post_stim)
 fprintf(1, '\n\nEpoching the data')
 
 for iTask = 1:numel(tasks)
@@ -187,221 +415,4 @@ for iTask = 1:numel(tasks)
 end
 
 fprintf(1, '\nDone!\n\n')
-
-%% Plots the PSTH
-clc
-close all
-
-opt.bin_size = 20;
-
-opt.mov_mean = 1;
-opt.moving_win_size = 20;
-
-opt.max_y_axis = ones(2,1)*.15;
-
-opt.plot_subj = 0;
-opt.normalize = 0;
-opt.visible = 'on';
-fontsize = 12;
-
-
-opt = get_option(opt);
-stim_color_mat = opt.stim_color_mat;
-stim_color_mat(stim_color_mat==0) = .8;
-stim_linestyle = opt.stim_linestyle;
-stim_legend = opt.stim_legend;
-samp_freq = opt.samp_freq;
-
-blnd_color = opt.blnd_color;
-sighted_color = opt.sighted_color;
-black = [0 0 0];
-
-bin_duration  = 1/samp_freq * opt.bin_size;
-
-x1_patch = pre_stim/opt.bin_size;
-x2_patch = (pre_stim+stim_dur)/opt.bin_size;
-
-% just to prevent label_your_axes from trying to give a meaningful scale
-opt.norm_resp = 1;
-
-
-for iGroup = 1:2
-    
-    if iGroup==1
-        Color = blnd_color/255;
-    else
-        Color = sighted_color/255;
-    end
-    
-    for iTask = 1:2
-        
-        figure('name', ['PSTH - ' tasks{iTask}], ...
-            'position', [50 50 1300 700], ...
-            'visible', opt.visible)
-        
-        iSubplot = 1;
-        
-        for iResp = 1:2
-            for iTrialtype = 1:4
-                
-                to_plot = cat(2, ...
-                    prestim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype),...
-                    stim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype),...
-                    poststim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype) );
-                
-                opt.nb_subj = size(to_plot,2);
-                
-                nb_bins = round(length(to_plot)/opt.bin_size);
-                x_tick = round(linspace(0,nb_bins,10));
-                x_label = round(linspace(0,nb_bins*bin_duration,10)) - pre_stim/samp_freq;
-                
-                [to_plot, sem, all_subjs] = process_timeSeries(to_plot, nb_bins, opt);
-                
-                subplot(2,4, iSubplot)
-                hold on
-                
-                patch(...
-                    [x1_patch x1_patch x2_patch x2_patch],...
-                    [0 opt.max_y_axis(1) opt.max_y_axis(1) 0],...
-                    stim_color_mat(iTrialtype,:),...
-                    'linestyle', stim_linestyle{iTrialtype});
- 
-                do_plot(to_plot, sem, all_subjs, Color, 1, opt)
-
-                plot([0 numel(to_plot)], [0 0], 'k')
-                
-                label_your_axes(fontsize, x_tick, x_label, opt);
-                
-                axis tight
-                ax = axis;
-                axis([ax(1) ax(2) ax(3) opt.max_y_axis(1)]);
-                
-                iSubplot = iSubplot + 1;
-            end
-            
-        end
-        
-        subplot(2, 4, 1)
-        t = ylabel('Resp 1  - [A U]');
-        set(t, 'fontsize', fontsize);
-        
-        subplot(2, 4, 5)
-        t = ylabel('Resp 2 - [A U]');
-        set(t, 'fontsize', fontsize);
-        
-        for iTrialtype = 1:4
-            subplot(2, 4, iTrialtype)
-            title(stim_legend{iTrialtype});
-        end
-        
-        mtit(sprintf('Group: %s ; Task: %s ; Bin size: %i ; Mov win size: %i', ...
-            group(iGroup).name, tasks{iTask}, opt.bin_size, opt.moving_win_size),....
-            'fontsize', 14, 'xoff', 0,'yoff',0.05);
-        
-        print(gcf, fullfile(out_dir, ...
-            ['PSTH_' ...
-            group(iGroup).name ...
-            '_task-' tasks{iTask} ...
-            '_rmbase-' num2str(rm_baseline) ...
-            '.jpg']), '-djpeg')
-        
-    end
 end
-
-
-%% Plots the average number of response during stimulus period
-clc
-close all
-
-max_y_axis = 6.5;
-
-opt.visible = 'on';
-fontsize = 12;
-
-opt = get_option(opt);
-stim_color_mat = opt.stim_color_mat;
-stim_linestyle = opt.stim_linestyle;
-stim_legend = opt.stim_legend;
-Colors(1,:) = opt.blnd_color;
-Colors(2,:) = opt.sighted_color;
-Colors = Colors/255;
-
-for iTask = 1:2
-    
-    figure('name', tasks{iTask}, 'position', [50 50 1300 700], 'visible', opt.visible)
-    
-    iSubplot = 1;
-    
-    for iResp = 1:2
-        for iTrialtype = 1:4
-            
-            clear to_plot
-            
-            subplot(2,4, iSubplot)
-            hold on
-            
-            for iGroup = 1:2
-                
-                to_plot = sum(stim_epoch{iResp,iGroup,iTask}(:,:,iTrialtype),2);
-                
-                h = plotSpread(to_plot, 'distributionIdx', ones(size(to_plot)), ...
-                    'distributionMarkers',{'o'},...
-                    'distributionColors',Colors(iGroup,:), ...
-                    'xValues', iGroup, ...
-                    'showMM', 0, ...
-                    'binWidth', .1, 'spreadWidth', 1);
-
-                set(h{1}, 'MarkerSize', 7, 'MarkerEdgeColor', 'k', ...
-                    'MarkerFaceColor', Colors(iGroup,:), ...
-                    'LineWidth', 2)
-                
-                h = errorbar(iGroup-.5, ...
-                    mean(to_plot), ...
-                    std(to_plot)/numel(to_plot)^.5,...
-                    'o',...
-                    'color', Colors(iGroup,:), 'linewidth', 2,...
-                    'MarkerSize', 5, ...
-                    'MarkerEdgeColor', 'k', ...
-                    'MarkerFaceColor', Colors(iGroup,:));
- 
-            end
-           
-            plot([0 3], [0 0], 'k')
-            
-            set(gca, 'fontsize', fontsize, ...
-                'ytick', 0:1:6, 'yticklabel', 0:1:6, ...
-                'xtick', 1:2, 'xticklabel', {group.name}, ...
-                'ticklength', [.02 .02], 'tickdir', 'out')
-            
-            axis([0.2 2.5 0 max_y_axis]);
-            
-            iSubplot = iSubplot + 1;
-        end
-        
-    end
-    
-    subplot(2, 4, 1)
-    t = ylabel('Resp 1  - [A U]');
-    set(t, 'fontsize', fontsize);
-    
-    subplot(2, 4, 5)
-    t = ylabel('Resp 2 - [A U]');
-    set(t, 'fontsize', fontsize);
-    
-    for iTrialtype = 1:4
-        subplot(2, 4, iTrialtype)
-        title(stim_legend{iTrialtype});
-    end
-    
-    mtit(sprintf('Mean number of responses during stim epoch ; Task: %s', tasks{iTask}),....
-        'fontsize', 14, 'xoff', 0,'yoff',0.05);
-    
-    print(gcf, fullfile(out_dir, ...
-        ['Avg_Resp_' ...
-        group(iGroup).name ...
-        '_task-' tasks{iTask} ...
-        '_rmbase-' num2str(rm_baseline) ...
-        '.jpg']), '-djpeg')
-    
-end
-

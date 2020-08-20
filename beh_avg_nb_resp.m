0d987a8ac69ee0bd68d84a4d9d4c81122f4421c9
Remi Gau <remi_gau@hotmail.com>
2020-08-20 18:42:52 +0200

    simplify figure for average number of responses

--- beh_avg_nb_resp.m
+++ beh_avg_nb_resp.m
@@ -32,7 +32,7 @@ opt.post_stim = 16;
 opt.rm_baseline = 0;
 
 % some settings for the figures
-max_y_axis = 6.5;
+max_y_axis = 25;
 
 opt.visible = 'on';
 
@@ -46,89 +46,107 @@ opt = get_option(opt);
 [~, stim_epoch, ~, group, tasks] = get_and_epoch_data(tgt_dir, opt);
 
 %% Plots the average number of response during stimulus period
-clc;
-close all;
 
 stim_legend = opt.stim_legend;
 Colors(1, :) = opt.blnd_color;
 Colors(2, :) = opt.sighted_color;
 Colors = Colors / 255;
 
+
+figure('name', 'Both tasks', 'position', [50 50 1300 700], 'visible', opt.visible);
+
 for iTask = 1:2
     
-    figure('name', tasks{iTask}, 'position', [50 50 1300 700], 'visible', opt.visible);
+    %% prepare data
     
-    iSubplot = 1;
-    
-    
-    for iTrialtype = 1:4
+    for iGroup = 1:2
         
-        clear to_plot;
-        
-        subplot(2, 4, iSubplot);
-        hold on;
-        
-        for iGroup = 1:2
-            
-            resp_1 = sum(stim_epoch{1, iGroup, iTask}(:, :, iTrialtype), 2);
-            resp_2 = sum(stim_epoch{1, iGroup, iTask}(:, :, iTrialtype), 2);
-            
-            h = plotSpread(to_plot, 'distributionIdx', ones(size(to_plot)), ...
-                'distributionMarkers', {'o'}, ...
-                'distributionColors', Colors(iGroup, :), ...
-                'xValues', iGroup, ...
-                'showMM', 0, ...
-                'binWidth', .1, 'spreadWidth', 1);
-            
-            set(h{1}, 'MarkerSize', 7, 'MarkerEdgeColor', 'k', ...
-                'MarkerFaceColor', Colors(iGroup, :), ...
-                'LineWidth', 2);
-            
-            h = errorbar(iGroup - .5, ...
-                mean(to_plot), ...
-                std(to_plot) / numel(to_plot)^.5, ...
-                'o', ...
-                'color', Colors(iGroup, :), 'linewidth', 2, ...
-                'MarkerSize', 5, ...
-                'MarkerEdgeColor', 'k', ...
-                'MarkerFaceColor', Colors(iGroup, :));
-            
+        switch tasks{iTask}
+            case 'olfid'
+                
+                % Good responses
+                temp_1 = cat(2, ...
+                    stim_epoch{1, iGroup, iTask}(:, :, [1, 3]), ... % left finger for eucalyptus
+                    stim_epoch{2, iGroup, iTask}(:, :, [2, 4])); % right finger for almond
+                
+                % Bad responses
+                temp_2 = cat(2, ...
+                    stim_epoch{2, iGroup, iTask}(:, :, [1, 3]), ... % right finger for eucalyptus
+                    stim_epoch{1, iGroup, iTask}(:, :, [2, 4])); % left finger for almond
+                
+            case 'olfloc'
+
+                % Good responses (left finger for left nostril)
+                temp_1 = cat(2, ...
+                    stim_epoch{1, iGroup, iTask}(:, :, [1, 2]), ... % left finger for left nostril
+                    stim_epoch{2, iGroup, iTask}(:, :, [3, 4])); % right finger for right nostril
+                
+                % Bad responses
+                temp_2 = cat(2, ...
+                    stim_epoch{2, iGroup, iTask}(:, :, [1, 2]), ... % left finger for right nostril
+                    stim_epoch{1, iGroup, iTask}(:, :, [3, 4])); % right finger for left nostril
+                
         end
         
-        plot([0 3], [0 0], 'k');
+        good_responses{iGroup} = sum(sum(temp_1, 3), 2);
+        bad_responses{iGroup} = sum(sum(temp_2, 3), 2);
         
-        set(gca, 'fontsize', fontsize, ...
-            'ytick', 0:1:6, 'yticklabel', 0:1:6, ...
-            'xtick', 1:2, 'xticklabel', {group.name}, ...
-            'ticklength', [.02 .02], 'tickdir', 'out');
-        
-        axis([0.2 2.5 0 max_y_axis]);
-        
-        iSubplot = iSubplot + 1;
     end
     
+    subplot(1, 2, iTask);
+    hold on;
     
-    subplot(2, 4, 1);
-    t = ylabel('Resp 1  - [A U]');
-    set(t, 'fontsize', fontsize);
-    
-    subplot(2, 4, 5);
-    t = ylabel('Resp 2 - [A U]');
-    set(t, 'fontsize', fontsize);
-    
-    for iTrialtype = 1:4
-        subplot(2, 4, iTrialtype);
-        title(stim_legend{iTrialtype});
+    for iGroup = 1:2
+        
+        to_plot = good_responses{iGroup} - bad_responses{iGroup};
+        
+        h = plotSpread(to_plot, 'distributionIdx', ones(size(to_plot)), ...
+            'distributionMarkers', {'o'}, ...
+            'distributionColors', Colors(iGroup, :), ...
+            'xValues', iGroup, ...
+            'showMM', 0, ...
+            'binWidth', .1, 'spreadWidth', 1);
+        
+        set(h{1}, 'MarkerSize', 7, 'MarkerEdgeColor', 'k', ...
+            'MarkerFaceColor', Colors(iGroup, :), ...
+            'LineWidth', 2);
+        
+        h = errorbar(iGroup - .5, ...
+            nanmean(to_plot), ...
+            nanstd(to_plot) / numel(to_plot)^.5, ...
+            'o', ...
+            'color', Colors(iGroup, :), 'linewidth', 2, ...
+            'MarkerSize', 5, ...
+            'MarkerEdgeColor', 'k', ...
+            'MarkerFaceColor', Colors(iGroup, :));
+        
     end
     
-    mtit(sprintf('Mean number of responses during stim epoch ; Task: %s', tasks{iTask}), ....
-        'fontsize', 14, 'xoff', 0, 'yoff', 0.05);
+    plot([0 3], [0 0], 'k');
     
-    print(gcf, fullfile(out_dir, ...
-        ['Avg_Resp_' ...
-        group(iGroup).name ...
-        '_task-' tasks{iTask} ...
-        '_rmbase-' num2str(opt.rm_baseline) ...
-        '.jpg']), '-djpeg');
+    set(gca, 'fontsize', fontsize, ...
+        'ytick', -25:5:25, 'yticklabel', -25:5:25, ...
+        'xtick', 1:2, 'xticklabel', {group.name}, ...
+        'ticklength', [.02 .02], 'tickdir', 'out');
     
+    title(['task: ' tasks{iTask}]);
+    
+    axis([0.2 2.5 -max_y_axis max_y_axis]);
+    
+    subplot(1, 2, 1);
+    t = ylabel(sprintf(...
+        ['     (Good response  - Bad response) ; [A U]\n\n', ...
+        'More bad responses <--      --> More good responses\n\n']));
+    set(t, 'fontsize', fontsize);
+
 end
+
+mtit(sprintf('Mean number of responses during stim epoch '), ....
+    'fontsize', 14, 'xoff', 0, 'yoff', 0.05);
+
+print(gcf, fullfile(out_dir, ...
+    ['Avg_Resp_' ...
+    group(iGroup).name ...
+    '_task-' tasks{iTask} ...
+    '_rmbase-' num2str(opt.rm_baseline) ...
+    '.jpg']), '-djpeg');

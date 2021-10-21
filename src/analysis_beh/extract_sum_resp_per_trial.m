847eb13822768327a9640664fd3b4ce0d12a919a
Remi Gau <remi_gau@hotmail.com>
2021-10-21 10:09:58 +0200

    fix issues in data extraction

--- src/analysis_beh/extract_sum_resp_per_trial.m
+++ src/analysis_beh/extract_sum_resp_per_trial.m
@@ -58,66 +58,47 @@ for iTask = 1:2
 
             % trials
             eucalyptus = [1, 3];
-            almond = [2, 4];
+            is_eucalyptus = ismember(trial_id, eucalyptus);
 
-            % good: left finger for eucalyptus
-            good.resp(1) = right;
-            good.trials{1} = eucalyptus;
-            % good: right finger for almond
-            good.resp(2) = left;
-            good.trials{2} = almond;
-
-            % bad: right finger for eucalyptus
-            bad.resp(1) = right;
-            bad.trials{1} = eucalyptus;
-            % bad: left finger for almond
-            bad.resp(2) = left;
-            bad.trials{2} = almond;
+            if is_eucalyptus
+              % good: left finger for eucalyptus
+              % bad: right finger for eucalyptus
+              good_response = stim_epoch{left, iGroup, iTask}(iSub, :, trial_id, run_id);
+              bad_response = stim_epoch{right, iGroup, iTask}(iSub, :, trial_id, run_id);
+            else
+              % good: right finger for almond
+              % bad: left finger for almond
+              good_response = stim_epoch{right, iGroup, iTask}(iSub, :, trial_id, run_id);
+              bad_response = stim_epoch{left, iGroup, iTask}(iSub, :, trial_id, run_id);
+            end
 
           elseif strcmp(tasks{iTask}, 'olfloc')
 
             % trials
             left_nostril = [1, 2];
-            right_nostril = [3, 4];
+            is_left_nostril = ismember(trial_id, left_nostril);
 
-            % good responses
-            good.resp(1) = right;
-            good.trials{1} = right_nostril;
-
-            good.resp(2) = left;
-            good.trials{2} = left_nostril;
-
-            % bad responses
-            bad.resp(1) = right;
-            bad.trials{1} = left_nostril;
-
-            bad.resp(2) = left;
-            bad.trials{2} = right_nostril;
+            if is_left_nostril
+              % good: left finger for left_nostril
+              % bad: right finger for right_nostril
+              good_response = stim_epoch{left, iGroup, iTask}(iSub, :, trial_id, run_id);
+              bad_response = stim_epoch{right, iGroup, iTask}(iSub, :, trial_id, run_id);
+            else
+              % good: right finger for left_nostril
+              % bad: left finger for right_nostril
+              good_response = stim_epoch{right, iGroup, iTask}(iSub, :, trial_id, run_id);
+              bad_response = stim_epoch{left, iGroup, iTask}(iSub, :, trial_id, run_id);
+            end
 
           end
-
-          %% reorganize data
-
-          % Good responses
-          temp_1 = cat(2, ...
-                       stim_epoch{good.resp(1), iGroup, iTask}(iSub, :, good.trials{1}, run_id), ...
-                       stim_epoch{good.resp(2), iGroup, iTask}(iSub, :, good.trials{2}, run_id));
-
-          % Bad responses
-          temp_2 = cat(2, ...
-                       stim_epoch{bad.resp(1), iGroup, iTask}(iSub, :, bad.trials{1}, run_id), ...
-                       stim_epoch{bad.resp(2), iGroup, iTask}(iSub, :, bad.trials{2}, run_id));
-
-          good_response = sum(temp_1(:));
-          bad_response = sum(temp_2(:));
 
           output.group_id{row} = group_id;
           output.sub_id{row} = sub_id;
           output.task_id{row} = task_id;
           output.run_id(row) = run_id;
           output.trial_id(row) = trial_id;
-          output.good_response(row) = good_response;
-          output.bad_response(row) = bad_response;
+          output.good_response(row) = sum(good_response);
+          output.bad_response(row) = sum(bad_response);
 
           row = row + 1;
 

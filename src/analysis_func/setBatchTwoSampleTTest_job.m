e1f4a11e40f30a384e37161a17af965333f65085
Remi Gau <remi_gau@hotmail.com>
2022-05-30 21:01:29 +0200

    add draft for 2 sample ttest

---
+++ src/analysis_func/setBatchTwoSampleTTest_job.m
@@ -0,0 +1,20 @@
+%-----------------------------------------------------------------------
+% Job saved on 30-May-2022 20:59:45 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/home/remi/gin/Christine/olfaction_blind/derivatives/cpp_spm/code/src/analysis_func'};
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = {'/home/remi/gin/Christine/olfaction_blind/derivatives/cpp_spm/outputs/derivatives/cpp_spm-stats/sub-blnd01/task-olfidolfloc_space-MNI152NLin2009cAsym_FWHM-6_desc-TissueConfounds/beta_0001.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = {'/home/remi/gin/Christine/olfaction_blind/derivatives/cpp_spm/outputs/derivatives/cpp_spm-stats/sub-ctrl01/task-olfidolfloc_space-MNI152NLin2009cAsym_FWHM-6_desc-TissueConfounds/beta_0001.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.des.t2.dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {'/home/remi/gin/Christine/olfaction_blind/derivatives/cpp_spm/outputs/derivatives/cpp_spm-stats/derivatives/cpp_spm-groupStats/space-MNI152NLin2009cAsym_desc-brain.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;

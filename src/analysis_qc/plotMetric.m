de7ec44984c4a639511734c0dce36b313f625581
Remi Gau <remi_gau@hotmail.com>
2022-06-04 20:48:15 +0200

    [DATALAD] Recorded changes

---
+++ src/analysis_qc/plotMetric.m
@@ -0,0 +1,20 @@
+function plotMetric(valuesToPlot, outliers, metricName, fileLists)
+  % (C) Copyright 2021 Remi Gau
+
+  metricName = strrep(metricName, '_', ' ');
+
+  figure('name', metricName);
+
+  hold on;
+
+  bar(1.5:numel(valuesToPlot) + .5, valuesToPlot);
+  plot(find(outliers), valuesToPlot(logical(outliers)), ' or');
+
+  xlabel('files');
+  ylabel(metricName);
+
+  set(gca, 'xtick', 1:size(fileLists, 1), ...
+      'xticklabel', fileLists, ...
+      'fontsize', 8);
+
+end
--- src/analysis_qc/quality_control_MRIQC.m
+++ src/analysis_qc/quality_control_MRIQC.m
@@ -1,3 +1,5 @@
+% (C) Copyright 2021 Remi Gau
+
 opt = options();
 
 opt.dir.mriqc = '/home/remi/gin/olfaction_blind/mriqc';

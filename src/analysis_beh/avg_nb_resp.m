6262b321686f69f82f84ab32b336f2542788cd92
Remi Gau <remi_gau@hotmail.com>
2022-06-05 21:14:58 +0200

    refactor beh figure plotting

--- src/analysis_beh/avg_nb_resp.m
+++ src/analysis_beh/avg_nb_resp.m
@@ -37,21 +37,6 @@ group_tsv_file = fullfile(output_dir, ...
 out_dir = fullfile(output_dir, 'beh', 'figures', 'beh_avg');
 spm_mkdir(out_dir);
 
-% some settings for the figures
-max_y_axis = [-4, 6];
-
-x_values_for_mean = [0.75, 2.25];
-
-fontsize = 12;
-
-spaghetti_plot =  true;
-
-Colors(1, :) = opt.blnd_color;
-Colors(2, :) = opt.sighted_color;
-Colors = Colors / 255;
-
-stim_legend = opt.stim_legend;
-
 %% Create summary table
 
 data = bids.util.tsvread(input_file);
@@ -90,7 +75,7 @@ for iTask = 1:2
       summary.group_id(row) = groups(iGroup);
       summary.task_id(row) = tasks(iTask);
       summary.good_minus_bad(row) = mean(data.good_response(rows_to_keep)) - ...
-          mean(data.bad_response(rows_to_keep));
+        mean(data.bad_response(rows_to_keep));
 
       row = row + 1;
 
@@ -100,14 +85,75 @@ for iTask = 1:2
 
 end
 
-% bids.util.tsvwrite(output_tsv_file, summary);
+bids.util.tsvwrite(output_tsv_file, summary);
+
+%% Plot
+clear data;
+
+Colors(1, :) = opt.blnd_color;
+Colors(2, :) = opt.sighted_color;
+
+source_data = bids.util.tsvread(output_tsv_file);
+
+figure('name', 'Both tasks', 'position', [50 50 1300 700], 'visible', opt.visible);
+
+tasks = unique(source_data.task_id);
+groups = unique(source_data.group_id);
+
+for i_group = 1:2
+
+  is_in_group = ismember(source_data.group_id,  groups(i_group));
+
+  for iTask = 1:2
+
+    is_task = ismember(source_data.task_id,  tasks(iTask));
+
+    rows_to_keep = all([is_task, is_in_group], 2);
+
+    data{i_group}(:, iTask) = source_data.good_minus_bad(rows_to_keep);
+
+  end
+
+end
+
+yMin = min(cellfun(@(x) min(x(:)), data));
+yMax = max(cellfun(@(x) max(x(:)), data));
+
+groups = {'blind', 'control'};
+
+for i_group = 1:2
+
+  subplot(1, 2, i_group);
+
+  if i_group == 1
+    yLabel = sprintf(['     (Good response  - Bad response) ; [A U]\n\n', ...
+                      'More bad responses <--      --> More good responses\n\n']);
+  else
+    yLabel = '';
+  end
+
+  spaghetti_plot(data{i_group}, ...
+                 'spaghetti', true, ...
+                 'fontSize', 10, ...
+                 'xTickLabel', {'identification', 'localization'}, ...
+                 'xLabel', 'tasks', ...
+                 'yLabel', yLabel, ...
+                 'yMin', yMin, ...
+                 'yMax', yMax, ...
+                 'title', sprintf('%s (n=%i)', groups{i_group}, size(data{i_group}, 1)), ...
+                 'markerSize', 8, ...
+                 'color', Colors(i_group, :));
+
+end
+
+print(gcf, fullfile(out_dir, ...
+                    ['grp-AvgResp_rmbase-' num2str(opt.rm_baseline) ...
+                     '_plot.jpg']), '-djpeg');
 
 %% Plots the average number of response during stimulus period
 clear data;
 
 data = bids.util.tsvread(output_tsv_file);
-
-figure('name', 'Both tasks', 'position', [50 50 1300 700], 'visible', opt.visible);
 
 tasks = unique(data.task_id);
 groups = unique(data.group_id);
@@ -118,44 +164,15 @@ summary = struct();
 
 for iGroup = 1:2
 
-  subplot(1, 2, iGroup);
-  hold on;
-
-  spaghetti = [];
-
   for iTask = 1:2
 
     is_task = ismember(data.task_id,  tasks(iTask));
+
     is_in_group = ismember(data.group_id,  groups(iGroup));
 
     rows_to_keep = all([is_task, is_in_group], 2);
 
     to_plot = data.good_minus_bad(rows_to_keep);
-
-    if ~spaghetti_plot
-      h = plotSpread(to_plot, 'distributionIdx', ones(size(to_plot)), ...
-                     'distributionMarkers', {'o'}, ...
-                     'distributionColors', Colors(iGroup, :), ...
-                     'xValues', iTask, ...
-                     'showMM', 0, ...
-                     'binWidth', .1, 'spreadWidth', 1);
-
-      set(h{1}, 'MarkerSize', 7, 'MarkerEdgeColor', 'k', ...
-          'MarkerFaceColor', Colors(iGroup, :), ...
-          'LineWidth', 2);
-    else
-      spaghetti(iTask, :) = to_plot;
-
-    end
-
-    errorbar(x_values_for_mean(iTask), ...
-             nanmean(to_plot), ...
-             nanstd(to_plot) / numel(to_plot)^.5, ...
-             'o', ...
-             'color', Colors(iGroup, :), 'linewidth', 2, ...
-             'MarkerSize', 5, ...
-             'MarkerEdgeColor', 'k', ...
-             'MarkerFaceColor', Colors(iGroup, :));
 
     summary.group_id(row) = groups(iGroup);
     summary.task_id(row) = tasks(iTask);
@@ -166,40 +183,6 @@ for iGroup = 1:2
 
   end
 
-  if spaghetti_plot
-    h = plot(1:2, spaghetti, 'o-');
-    for i = 1:numel(h)
-      set(h(i), 'color', Colors(iGroup, :), 'linewidth', 1, ...
-          'MarkerSize', 5, ...
-          'MarkerEdgeColor', 'k', ...
-          'MarkerFaceColor', 'k');
-    end
-  end
-
-  plot([0 3], [0 0], 'k');
-
-  set(gca, 'fontsize', fontsize, ...
-      'ytick', -25:2.5:25, 'yticklabel', -25:2.5:25, ...
-      'xtick', 1:2, 'xticklabel', tasks, ...
-      'ticklength', [.02 .02], 'tickdir', 'out');
-
-  title(groups(iGroup));
-
-  axis([x_values_for_mean(1) - 0.2, x_values_for_mean(2) + 0.2,  max_y_axis]);
-
-  subplot(1, 2, 1);
-  t = ylabel(sprintf( ...
-                     ['     (Good response  - Bad response) ; [A U]\n\n', ...
-                      'More bad responses <--      --> More good responses\n\n']));
-  set(t, 'fontsize', fontsize);
-
 end
 
 bids.util.tsvwrite(group_tsv_file, summary);
-
-mtit(sprintf('Mean number of responses during stim epoch '), ....
-     'fontsize', 14, 'xoff', 0, 'yoff', 0.05);
-
-% print(gcf, fullfile(out_dir, ...
-%                     ['grp-AvgResp_rmbase-' num2str(opt.rm_baseline) ...
-%                      '_plot.jpg']), '-djpeg');

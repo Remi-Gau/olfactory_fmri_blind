b43220152f246ca07b5f9a7ee0db53ec1860cb08
Remi Gau <remi_gau@hotmail.com>
2022-06-06 08:59:22 +0200

    move reports folder

---
+++ reports/write_reports.m
@@ -0,0 +1,34 @@
+% (C) Copyright 2021 Remi Gau
+
+% write method description for dataset
+
+clear;
+clc;
+
+run ../initEnv.m;
+
+%%
+close all;
+
+opt = opt_dir();
+opt.dir.input = opt.dir.raw;
+opt.dir.output = pwd;
+
+opt.subjects = 'ctrl01';
+opt.query.modality = {'anat', 'func'};
+
+reportBIDS(opt);
+
+%%
+clear opt;
+opt.model.file = fullfile(fileparts(mfilename('fullpath')), '..', ...
+                          'analysis_func', ...
+                          'models', ...
+                          'model-TissueConfounds_smdl.json');
+opt.fwhm.contrast = 0;
+opt = checkOptions(opt);
+
+opt.designType = 'block';
+
+outputFile = boilerplate(opt, 'outputPath', pwd, ...
+                         'pipelineType', 'stats');
